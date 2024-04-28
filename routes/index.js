676f5743bd8965477b5611088dd429061a4f46e4
Your Name <you@example.comkanchanvishwakarma555555@gmail.com>
2024-04-28 16:51:03 +0530

    update and delet

--- routes/index.js
+++ routes/index.js
@@ -3,52 +3,88 @@ var router = express.Router();
 
 const bookmodel = require("../models/bookmodel")
 
-// var tanisha_book =[]
+// var tanisha_book = []
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', async function (req, res, next) {
+  const Allbook = await bookmodel.find()
+  res.render('index', { Allbook });
 });
 
-router.get('/create', async function(req, res, next) {
+router.get('/create', async function (req, res, next) {
   const Allbook = await bookmodel.find()
   console.log(Allbook)
-  res.render('create',{Allbook});
+  res.render('create', { Allbook });
 });
 
-router.post('/create', async function(req, res, next) {
-const newbook = await bookmodel.create({
-  name:req.body.name,
-  author:req.body.author,
-  price:req.body.price,
-  language:req.body.language,
-})
+router.post('/create', async function (req, res, next) {
+  const newbook = await bookmodel.create({
+    name: req.body.name,
+    author: req.body.author,
+    price: req.body.price,
+    language: req.body.language,
+  })
 
-console.log(newbook)
+  console.log(newbook)
 
-res.redirect("/readall")
-});
-
-router.get('/readall', async function(req, res, next) {
-  const Allbook = await bookmodel.find()
-  res.render('library' ,{abd:Allbook});
-});
-
-router.get('/delete/:index', function(req, res, next) {
-tanisha_book.splice(req.params.index,1)
-res.redirect("/readall")
-});
-
-router.get('/update/:index', function(req, res, next) {
-const i = req.params.index
-const b = tanisha_book[i]
-res.render("update",{gogo:b,index:i})
-});
-
-router.post('/update/:index', function(req, res, next) {
-  const i = req.params.index
-  tanisha_book[i] = req.body 
   res.redirect("/readall")
 });
+
+router.get('/readall', async function (req, res, next) {
+  const Allbook = await bookmodel.find()
+  res.render('library', { abd: Allbook });
+});
+
+router.get('/delete/:index', function (req, res, next) {
+  tanisha_book.splice(req.params.index, 1)
+  res.redirect("/readall")
+});
+
+router.get('/update/:index', async function (req, res, next) {
+  // const i = req.params.index
+  // const b = tanisha_book[i]
+  // res.render("update",{gogo:b,index:i})
+
+  const index = req.params.index
+  const currentbook = await bookmodel.find({
+    _id: index
+  })
+  res.render("update", { currentbook })
+  console.log(currentbook)
+});
+
+router.post('/update/:index', async function (req, res, next) {
+  // const i = req.params.index
+  // tanisha_book[i] = req.body 
+  // res.redirect("/readall")
+
+  const index = req.params.index
+
+  await bookmodel.findOneAndUpdate(
+    {
+      _id: index
+    }, {
+
+    name: req.body.name,
+    author: req.body.author,
+    price: req.body.price,
+    language: req.body.language,
+            
+  })
+
+  res.redirect("/readall")
+
+});
+
+router.get("/delete/:index", async (req,res,next)=>{
+  const index = req.params.index
+
+  await bookmodel.findOneAndDelete({
+    _id: index
+  })
+  res.redirect("/readall")
+})
+
+
 
 
 module.exports = router;
